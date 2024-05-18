dcd69082a932d461ea16488d6a74ed7bff55e306
LukasKlenner <klenner-lukas@t-online.de>
2024-05-18 12:02:26 +0200

    wip: finalize RadixSortTest

--- src/graderPrivate/java/p1/RadixSortTest.java
+++ src/graderPrivate/java/p1/RadixSortTest.java
@@ -5,15 +5,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junitpioneer.jupiter.json.JsonClasspathSource;
 import org.junitpioneer.jupiter.json.Property;
+import org.mockito.ArgumentCaptor;
 import org.mockito.exceptions.base.MockitoAssertionError;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
 import org.tudalgo.algoutils.tutor.general.assertions.Context;
 import p1.sort.ArraySortList;
 import p1.sort.SortList;
+import p1.sort.radix.Bucket;
 import p1.sort.radix.IntegerIndexExtractor;
 import p1.sort.radix.RadixSort;
 import p1.transformers.MethodInterceptor;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -33,25 +36,107 @@ public class RadixSortTest {
 
     @ParameterizedTest
     @JsonClasspathSource(value = "H7_RadixSortTests.json", data = "putBucketTest")
-    public void testPutBucket(@Property("value") Integer value, @Property("index") Integer index) {
-        IntegerIndexExtractor extractor = spy(new IntegerIndexExtractor(10));
-        RadixSort<Integer> radixSort = spy(new RadixSort<>(10, extractor));
+    public void testPutBucket(@Property("value") Integer value, @Property("position") Integer position) throws ReflectiveOperationException {
+
+        int radix = 10;
+
+        IntegerIndexExtractor extractor = spy(new IntegerIndexExtractor(radix));
+        RadixSort<Integer> radixSort = spy(new RadixSort<>(radix, extractor));
         radixSort.setMaxInputLength(1);
+
+        int index = new IntegerIndexExtractor(radix).extractIndex(value, position);
 
         Context context = contextBuilder()
             .subject("RadixSort#putBucket()")
+            .add("value", value)
+            .add("position", position)
+            .add("index", index)
             .build();
 
-        radixSort.putBucket(value, index);
+        call(() -> radixSort.putBucket(value, position), context, TR -> "putBucket() should not throw an exception.");
 
-        checkVerify(() -> verify(extractor, times(1)).extractIndex(value, index), context,
-            "Extract Index was not called with the expected values.");
+        checkVerify(() -> verify(extractor, times(1)).extractIndex(value, position), context,
+            "extractIndex was not called with the expected values.");
+
+        Bucket<Integer>[] buckets = getBuckets(radixSort);
+
+        for (int i = 0; i < radix; i++) {
+            int finalI = i;
+
+            if (i == index) {
+                assertEquals(1, buckets[i].size(), context, result -> "Bucket at position %d should contain exactly one element.".formatted(finalI));
+                assertEquals(value, buckets[i].remove(), context, result -> "Bucket at position %d should contain the value %d.".formatted(finalI, value));
+            } else {
+                assertEquals(0, buckets[i].size(), context, result -> "Bucket at position %d should be empty.".formatted(finalI));
+            }
+
+        }
     }
 
     @ParameterizedTest()
-    @JsonClasspathSource(value = "H7_RadixSortTests.json", data = "singleBucketTest")
-    public void oneBucketEntryMaxLength1(@Property("values") List<Integer> values, @Property("expected") List<Integer> expected) {
-        testSorting(values, expected, 1, 1);
+    @JsonClasspathSource(value = "H7_RadixSortTests.json", data = "putBucketCallMaxLength1Test")
+    public void testPutBucketCallMaxLength1(@Property("values") List<Integer> values) {
+        testPutBucketCall(values, 1, 10);
+    }
+
+    @ParameterizedTest()
+    @JsonClasspathSource(value = "H7_RadixSortTests.json", data = "putBucketCallTest")
+    public void testPutBucketCall(@Property("values") List<Integer> values, @Property("maxInputLength") Integer maxInputLength) {
+        testPutBucketCall(values, maxInputLength, 10);
+    }
+
+    private void testPutBucketCall(List<Integer> values, int maxInputLength, int radix) {
+        Context context = contextBuilder()
+            .subject("RadixSort#sort()")
+            .add("values", values)
+            .add("maxInputLength", maxInputLength)
+            .build();
+
+        RadixSort<Integer> radixSort = spy(new RadixSort<>(radix, new IntegerIndexExtractor(radix)));
+        radixSort.setMaxInputLength(maxInputLength);
+
+        ArgumentCaptor<Integer> valueCaptor = ArgumentCaptor.forClass(Integer.class);
+        ArgumentCaptor<Integer> positionCaptor = ArgumentCaptor.forClass(Integer.class);
+        doNothing().when(radixSort).putBucket(valueCaptor.capture(), positionCaptor.capture());
+        doCallRealMethod().doNothing().when(radixSort).sort(any());
+
+        SortList<Integer> sortList = new ArraySortList<>(values);
+        call(() -> radixSort.sort(sortList), context, result -> "sort() should not throw an exception.");
+
+        List<Integer> capturedValues = valueCaptor.getAllValues();
+        List<Integer> capturedPositions = positionCaptor.getAllValues();
+
+        int expectedCalls = values.size() * maxInputLength;
+
+        if (capturedValues.size() != expectedCalls) {
+            fail(contextBuilder()
+                .subject("RadixSort#sort()")
+                .add("values", values)
+                .add("maxInputLength", maxInputLength)
+                .add("expected calls", expectedCalls)
+                .add("actual calls", capturedValues.size())
+                .build(), result -> "putBucket() was not called the expected amount of times.");
+        }
+
+        for (int i = 0; i < values.toArray().length; i++) {
+            int finalI = i;
+            assertEquals(values.get(i), capturedValues.get(i), context,
+                result -> "putBucket() was called with the wrong value at the %d-th call.".formatted(finalI + 1));
+            assertEquals(i / values.size(), capturedPositions.get(i), context,
+                result -> "putBucket() was called with the wrong position at the %d-th call.".formatted(finalI + 1));
+        }
+    }
+
+    @ParameterizedTest()
+    @JsonClasspathSource(value = "H7_RadixSortTests.json", data = "oneBucketOneEntryTest")
+    public void testOneBucketOneEntry(@Property("buckets") List<List<Integer>> buckets, @Property("expected") List<Integer> expected, @Property("radix") int radix) throws ReflectiveOperationException {
+        testBucketExtraction(buckets, expected, radix);
+    }
+
+    @ParameterizedTest()
+    @JsonClasspathSource(value = "H7_RadixSortTests.json", data = "oneBucketMultipleEntriesTest")
+    public void testOneBucketMultipleEntries(@Property("buckets") List<List<Integer>> buckets, @Property("expected") List<Integer> expected, @Property("radix") int radix) throws ReflectiveOperationException {
+        testBucketExtraction(buckets, expected, radix);
     }
 
     @ParameterizedTest()
@@ -64,6 +149,49 @@ public class RadixSortTest {
     @JsonClasspathSource(value = "H7_RadixSortTests.json", data = "oneBucketTest")
     public void testOneBucketMultipleEntries(@Property("values") List<Integer> values, @Property("expected") List<Integer> expected) {
         testSorting(values, expected, 1, 10);
+    }
+
+    private void testBucketExtraction(List<List<Integer>> buckets, List<Integer> expected, Integer radix) throws ReflectiveOperationException {
+        RadixSort<Integer> radixSort = spy(new RadixSort<>(radix, new IntegerIndexExtractor(radix)));
+        radixSort.setMaxInputLength(1);
+
+        doNothing().when(radixSort).putBucket(any(), anyInt());
+
+        Bucket<Integer>[] bucketArray = getBuckets(radixSort);
+
+        for (int i = 0; i < radix; i++) {
+            for (Integer value : buckets.get(i)) {
+                bucketArray[i].add(value);
+            }
+        }
+
+        Context context = contextBuilder()
+            .subject("RadixSort#sort()")
+            .add("buckets", buckets)
+            .add("expected", expected)
+            .build();
+
+        SortList<Integer> sortList = new ArraySortList<>(expected.size());
+        call(() -> radixSort.sort(sortList), context, result -> "sortBuckets() should not throw an exception.");
+
+        context = contextBuilder()
+            .subject("RadixSort#sort()")
+            .add("buckets", buckets)
+            .add("expected", expected)
+            .add("actual", sortList)
+            .build();
+
+        for (int i = 0; i < radix; i++) {
+            int finalI = i;
+            assertEquals(0, bucketArray[i].size(), context,
+                    result -> "Bucket at position %d should be empty.".formatted(finalI));
+        }
+
+        for (int i = 0; i < expected.size(); i++) {
+            int finalI = i;
+            assertEquals(expected.get(i), sortList.get(i), context,
+                    result -> "sortList contains wrong value at index %d.".formatted(finalI));
+        }
     }
 
     private void testSorting(List<Integer> values, List<Integer> expected, Integer maxInputLength, Integer radix) {
@@ -97,10 +225,17 @@ public class RadixSortTest {
     private void checkVerify(Runnable verifier, Context context, String msg) {
         try {
             verifier.run();
-        } catch (MockitoAssertionError e) {
+        } catch (AssertionError e) {
             fail(context, result -> msg + " Original error message:\n" + e.getMessage());
         } catch (Exception e) {
             fail(context, result -> "Unexpected Exception:\n" + e.getMessage());
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> Bucket<T>[] getBuckets(RadixSort<T> radixSort) throws ReflectiveOperationException {
+        Field field = RadixSort.class.getDeclaredField("buckets");
+        field.setAccessible(true);
+        return (Bucket<T>[]) field.get(radixSort);
     }
 }
